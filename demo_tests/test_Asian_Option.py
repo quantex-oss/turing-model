e844afa136e6761fbc16ac4ef805ed0d34bc4adf
wangyutingone <yuting.wang@iquantex.com>
2021-05-20 19:35:26 +0800

    新增test文件

---
+++ demo_tests/test_Asian_Option.py
@@ -0,0 +1,32 @@
+from tool import print_result
+from fundamental import PricingContext
+from turing_models.utilities.global_types import TuringOptionTypes
+from turing_models.instrument.eq_option import EqOption
+from turing_models.utilities.turing_date import TuringDate
+
+
+option = EqOption(option_type=TuringOptionTypes.ASIAN_CALL,
+                  expiration_date=TuringDate(12, 2, 2021),
+                  start_averaging_date=TuringDate(15, 2, 2020),
+                  strike_price=90,
+                  value_date=TuringDate(12, 2, 2020),
+                  stock_price=100,
+                  volatility=0.1,
+                  interest_rate=0.02,
+                  dividend_yield=0,
+                  accrued_average=None)
+
+
+print_result(option)
+
+
+with PricingContext(interest_rate=0.04):
+    print_result(option)
+
+
+with PricingContext(interest_rate=0.04):
+    print_result(option)
+
+
+with PricingContext(interest_rate=0.06):
+    print_result(option)
---
+++ demo_tests/tool.py
@@ -0,0 +1,11 @@
+from rich import print, style
+from rich.panel import Panel
+
+
+def print_result(option):
+    print(Panel.fit(f"price: {option.price()}\n"
+                    f"delta: {option.delta()}\n"
+                    f"gamma: {option.gamma()}\n"
+                    f"vega: {option.vega()}\n"
+                    f"theta: {option.theta()}\n"
+                    f"rho: {option.rho()}", title="Result"))
