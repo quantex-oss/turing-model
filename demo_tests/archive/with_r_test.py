c342ebeb6067cfcb50cd284e975c65daabcf9a8b
杨晓亮 <xiaoliang.yang>
2021-05-21 17:36:50 +0800

    change eqoption class

--- demo_tests/archive/with_r_test.py
+++ demo_tests/archive/with_r_test.py
@@ -5,26 +5,11 @@ from turing_models.instrument.eq_option import EqOption
 from turing_models.products.equity import TuringOptionTypes
 from turing_models.utilities.turing_date import TuringDate
 
-set_switch(0)
+set_switch(1)
 # --------------------------------------------------------------------------
 # Section 1: Pricing
 # --------------------------------------------------------------------------
 
-
-# option = EqOption(option_type=OptionType.Call,
-#                   option_style=OptionStyle.Snowball,
-#                   number_of_options=100,
-#                   expiration_date=datetime.date(2021, 2, 12),
-#                   knock_out_price=120,
-#                   knock_in_price=90,
-#                   notional=1000000,
-#                   coupon_rate=0.3,
-#                   value_date=datetime.date(2020, 2, 12),
-#                   stock_price=100,
-#                   volatility=0.1,
-#                   interest_rate=0.02,
-#                   dividend_yield=0,
-#                   knock_in_type=KnockInType.RETURN)
 
 option = EqOption(option_type=TuringOptionTypes.EUROPEAN_CALL,
                   number_of_options=100,
