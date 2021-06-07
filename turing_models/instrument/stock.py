7b75a2dbbd94f85f9c5ebdc710348adaecfae749
wangyutingone <yuting.wang@iquantex.com>
2021-06-07 10:26:15 +0800

    调整股票的price和greeks计算方法

--- turing_models/instrument/stock.py
+++ turing_models/instrument/stock.py
@@ -17,23 +17,18 @@ class Stock(Priceable):
     wind_id = StringField('wind_id')
     sedol = StringField('sedol')
     cusip = StringField('cusip')
-    quantity: float = FloatField('quantity')
+    quantity: float = FloatField('quantity')  # 股数
     stock_price: float = FloatField('stock_price')
 
     def __init__(self, **kw):
         super(Stock, self).__init__(**kw)
-        self.multiplier = 100
 
     def price(self):
-        """"计算一手股票的价格"""
-        return self.stock_price * self.multiplier
-
-    def value(self):
-        """"计算持仓现值"""
-        return self.stock_price * self.quantity * self.multiplier
+        """"计算持仓股票的现价"""
+        return self.stock_price * self.quantity
 
     def delta(self):
-        return self.multiplier
+        return self.quantity
 
     def gamma(self):
         return 0
