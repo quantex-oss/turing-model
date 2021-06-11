790e258759855e2f0227c2e3384e4016e5f54ad3
wangyutingone <yuting.wang@iquantex.com>
2021-06-11 14:54:53 +0800

    调整参数格式

--- demo_tests/eqoption_test.py
+++ demo_tests/eqoption_test.py
@@ -4,57 +4,60 @@ from fundamental import PricingContext
 from turing_models.instrument.eq_option import EqOption
 
 
-# european_option = EqOption(option_type='call',
-#                            product_type='European',
-#                            expiration_date=TuringDate(2021, 9, 3),
-#                            strike_price=5.3,
-#                            multiplier=10000,
-#                            value_date=TuringDate(2021, 6, 4),
-#                            stock_price=5.262,
-#                            volatility=0.1,
-#                            interest_rate=0.02,
-#                            dividend_yield=0)
+european_option = EqOption(option_type='call',
+                           product_type='European',
+                           expiration_date=TuringDate(2021, 9, 3),
+                           strike_price=5.3,
+                           quantity=2,
+                           multiplier=10000,
+                           value_date=TuringDate(2021, 6, 4),
+                           stock_price=5.262,
+                           volatility=0.1,
+                           interest_rate=0.02,
+                           dividend_yield=0)
 
 
-# american_option = EqOption(option_type='call',
-#                            product_type='American',
-#                            expiration_date=TuringDate(2021, 2, 12),
-#                            strike_price=90,
-#                            multiplier=10000,
-#                            value_date = TuringDate(2020, 2, 12),
-#                            stock_price=100,
-#                            volatility=0.1,
-#                            interest_rate=0.02,
-#                            dividend_yield=0)
+american_option = EqOption(option_type='call',
+                           product_type='American',
+                           expiration_date=TuringDate(2021, 2, 12),
+                           strike_price=90,
+                           quantity=2,
+                           multiplier=10000,
+                           value_date = TuringDate(2020, 2, 12),
+                           stock_price=100,
+                           volatility=0.1,
+                           interest_rate=0.02,
+                           dividend_yield=0)
 
-# asian_option = EqOption(option_type='call',
-#                         product_type='Asian',
-#                         expiration_date=TuringDate(2021, 2, 12),
-#                         start_averaging_date=TuringDate(2020, 2, 15),
-#                         strike_price=90,
-#                         value_date = TuringDate(2020, 2, 12),
-#                         multiplier=10000,
-#                         stock_price=100,
-#                         volatility=0.1,
-#                         interest_rate=0.02,
-#                         dividend_yield=0)
-#
-# snowball_option = EqOption(option_type='call',
-#                            product_type='Snowball',
-#                            expiration_date=TuringDate(2021, 2, 12),
-#                            participation_rate=1.0,
-#                            knock_out_price=120,
-#                            knock_in_price=90,
-#                            notional=1000000,
-#                            coupon_rate=0.2,
-#                            coupon_annualized_flag=True,
-#                            knock_in_type='Return',
-#                            value_date=TuringDate(2020, 2, 12),
-#                            multiplier=10000,
-#                            stock_price=100,
-#                            volatility=0.1,
-#                            interest_rate=0.02,
-#                            dividend_yield=0)
+asian_option = EqOption(option_type='call',
+                        product_type='Asian',
+                        expiration_date=TuringDate(2021, 2, 12),
+                        start_averaging_date=TuringDate(2020, 2, 15),
+                        strike_price=90,
+                        quantity=2,
+                        value_date = TuringDate(2020, 2, 12),
+                        multiplier=10000,
+                        stock_price=100,
+                        volatility=0.1,
+                        interest_rate=0.02,
+                        dividend_yield=0)
+
+snowball_option = EqOption(option_type='call',
+                           product_type='Snowball',
+                           expiration_date=TuringDate(2021, 2, 12),
+                           participation_rate=1.0,
+                           barrier=120,
+                           knock_in_price=90,
+                           notional=1000000,
+                           rebate=0.2,
+                           coupon_annualized_flag=True,
+                           knock_in_type='Return',
+                           value_date=TuringDate(2020, 2, 12),
+                           multiplier=10000,
+                           stock_price=100,
+                           volatility=0.1,
+                           interest_rate=0.02,
+                           dividend_yield=0)
 #
 knockout_option = EqOption(option_type='call',
                            product_type='Knockout',
@@ -74,19 +77,19 @@ knockout_option = EqOption(option_type='call',
                            dividend_yield=0)
 
 
-# print_result(european_option)
-# print_result(american_option)
-# print_result(asian_option)
-# print_result(snowball_option)
+print_result(european_option)
+print_result(american_option)
+print_result(asian_option)
+print_result(snowball_option)
 print_result(knockout_option)
-#
-#
-# with PricingContext(interest_rate=0.04):
-#     print_result(european_option)
-#     print_result(american_option)
-#     print_result(asian_option)
-#     print_result(snowball_option)
-#     print_result(knockout_option)
+
+
+with PricingContext(interest_rate=0.04):
+    print_result(european_option)
+    print_result(american_option)
+    print_result(asian_option)
+    print_result(snowball_option)
+    print_result(knockout_option)
 #
 #
 # with PricingContext(interest_rate=0.04):
