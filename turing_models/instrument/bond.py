4e88db32c87ce2e339d76aafae22c043319991da
wangyutingone <yuting.wang@iquantex.com>
2021-06-16 14:37:42 +0800

    完善bond

--- turing_models/instrument/bond.py
+++ turing_models/instrument/bond.py
@@ -21,9 +21,15 @@ class BondOrm(Priceable):
     bond_term_year: float = FloatField("bond_term_year")
     bond_term_day: float = FloatField("bond_term_day")
     maturity_date: TuringDate = DateField("due_date")
+    issue_date: TuringDate = DateField("issue_date")
+    freq_type = StringField('freq_type')
+    accrual_type = StringField('accrual_type')
+    face_amount: float = FloatField("face_amount")
+    clean_price: float = FloatField("clean_price")
+    ytm: float = FloatField("ytm")
     zero_dates: list = None
     zero_rates: list = None
-    ytm: float = FloatField("ytm")
+
 
     def __init__(self, **kw):
         super().__init__(**kw)
@@ -34,19 +40,20 @@ class BondOrm(Priceable):
 class Bond:
     asset_id: str = None
     bond_type: str = None
-    issue_date: TuringDate = None
-    maturity_date: TuringDate = None
     coupon: float = None
-    # quoted_margin: float = None
+    maturity_date: TuringDate = None
+    issue_date: TuringDate = None
     freq_type: str = None
     accrual_type: str = None
     face_amount: float = None
+    clean_price: float = None
     name: str = None
     settlement_date: TuringDate = TuringDate(*(datetime.date.today().timetuple()[:3]))
     ytm: float = None
     zero_dates: list = None
     zero_rates: list = None
-    clean_price: float = None
+    # 以下字段在浮息债中用到
+    # quoted_margin: float = None
     # next_coupon: float = None
     # current_ibor: float = None
     # future_ibor: float = None
@@ -54,39 +61,29 @@ class Bond:
 
     def __post_init__(self):
         self.name = 'No name'
-        if self.freq_type == 'annual':
+        if self.freq_type == '每年付息':
             self.freq_type = TuringFrequencyTypes.ANNUAL
-        elif self.freq_type == 'semi_annual':
+        elif self.freq_type == '半年付息':
             self.freq_type = TuringFrequencyTypes.SEMI_ANNUAL
-        elif self.freq_type == 'tri_annual':
+        elif self.freq_type == '4个月一次':
             self.freq_type = TuringFrequencyTypes.TRI_ANNUAL
-        elif self.freq_type == 'quarterly':
+        elif self.freq_type == '按季付息':
             self.freq_type = TuringFrequencyTypes.QUARTERLY
-        elif self.freq_type == 'monthly':
+        elif self.freq_type == '按月付息':
             self.freq_type = TuringFrequencyTypes.MONTHLY
         else:
             raise TuringError('Please check the input of freq_type')
 
-        if self.accrual_type == '30_360_bond':
-            self.accrual_type = TuringDayCountTypes.THIRTY_360_BOND
-        elif self.accrual_type == '30_e_360':
-            self.accrual_type = TuringDayCountTypes.THIRTY_E_360
-        elif self.accrual_type == '30_e_360_isda':
-            self.accrual_type = TuringDayCountTypes.THIRTY_E_360_ISDA
-        elif self.accrual_type == '30_e_plus_360':
-            self.accrual_type = TuringDayCountTypes.THIRTY_E_PLUS_360
-        elif self.accrual_type == 'act_act_isda':
-            self.accrual_type = TuringDayCountTypes.ACT_ACT_ISDA
-        elif self.accrual_type == 'act_act_icma':
-            self.accrual_type = TuringDayCountTypes.ACT_ACT_ICMA
-        elif self.accrual_type == 'act_365f':
-            self.accrual_type = TuringDayCountTypes.ACT_365F
-        elif self.accrual_type == 'act_360':
-            self.accrual_type = TuringDayCountTypes.ACT_360
-        elif self.accrual_type == 'act_365l':
+        if self.accrual_type == 'ACT/365':
             self.accrual_type = TuringDayCountTypes.ACT_365L
-        elif self.accrual_type == 'simple':
-            self.accrual_type = TuringDayCountTypes.SIMPLE
+        elif self.accrual_type == 'ACT/ACT':
+            self.accrual_type = TuringDayCountTypes.ACT_ACT_ISDA
+        elif self.accrual_type == 'ACT/360':
+            self.accrual_type = TuringDayCountTypes.ACT_360
+        elif self.accrual_type == '30/360':
+            self.accrual_type = TuringDayCountTypes.THIRTY_E_360
+        elif self.accrual_type == 'ACT/365F':
+            self.accrual_type = TuringDayCountTypes.ACT_365F
         else:
             raise TuringError('Please check the input of accrual_type')
 
