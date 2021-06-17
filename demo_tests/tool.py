b64d8271074c55aba6122a1bce6833ebe5ab137a
王雨婷 <yuting.wang@iquantex.com>
2021-06-17 14:18:36 +0800

    Update tool.py

--- demo_tests/tool.py
+++ demo_tests/tool.py
@@ -3,10 +3,10 @@ from rich.panel import Panel
 
 
 def print_result(option):
-    print(Panel.fit(f"price: {option.price()}\n"
-                    f"delta: {option.eq_delta()}\n"
-                    f"gamma: {option.eq_gamma()}\n"
-                    f"vega: {option.eq_vega()}\n"
-                    f"theta: {option.eq_theta()}\n"
-                    f"rho: {option.eq_rho()}\n"
-                    f"rho_q: {option.eq_rho_q()}", title="Result"))
+    print(Panel.fit(f"price: {round(option.price(), 3)}\n"
+                    f"delta: {round(option.eq_delta(), 3)}\n"
+                    f"gamma: {round(option.eq_gamma(), 3)}\n"
+                    f"vega: {round(option.eq_vega(), 3)}\n"
+                    f"theta: {round(option.eq_theta(), 3)}\n"
+                    f"rho: {round(option.eq_rho(), 3)}\n"
+                    f"rho_q: {round(option.eq_rho_q(), 3)}", title="Result"))
