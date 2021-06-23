78c8c3638e7c2db4c765ce975ab44beed462e5b1
wangyutingone <yuting.wang@iquantex.com>
2021-06-23 20:10:50 +0800

    新增EuropeanOption类

--- demo_tests/eqoption_test.py
+++ demo_tests/eqoption_test.py
@@ -1,107 +1,108 @@
 from turing_models.utilities.turing_date import TuringDate
+from turing_models.instrument.european_option import EuropeanOption
 from tool import print_result
 from fundamental.pricing_context import PricingContext
-from turing_models.instrument.eq_option import EqOption
-
-
-european_option = EqOption(option_type='CALL',
-                           product_type='EUROPEAN',
-                           expiration_date=TuringDate(2021, 9, 3),
-                           strike_price=5.3,
-                           number_of_options=2,
-                           multiplier=100,
-                           value_date=TuringDate(2021, 2, 3),
-                           stock_price=5.262,
-                           volatility=0.1,
-                           interest_rate=0.02,
-                           dividend_yield=0)
-
-
-american_option = EqOption(option_type='CALL',
-                           product_type='AMERICAN',
-                           expiration_date=TuringDate(2021, 9, 3),
-                           strike_price=5.3,
-                           number_of_options=2,
-                           multiplier=100,
-                           value_date=TuringDate(2021, 2, 3),
-                           stock_price=5.262,
-                           volatility=0.1,
-                           interest_rate=0.02,
-                           dividend_yield=0)
-
-asian_option = EqOption(option_type='CALL',
-                        product_type='ASIAN',
-                        expiration_date=TuringDate(2021, 9, 3),
-                        start_averaging_date=TuringDate(2021, 2, 15),
-                        strike_price=5.3,
-                        number_of_options=2,
-                        value_date=TuringDate(2021, 2, 3),
-                        multiplier=100,
-                        stock_price=5.262,
-                        volatility=0.1,
-                        interest_rate=0.02,
-                        dividend_yield=0)
-
-snowball_option = EqOption(option_type='CALL',
-                           product_type='SNOWBALL',
-                           expiration_date=TuringDate(2021, 9, 3),
-                           participation_rate=1.0,
-                           barrier=5.5,
-                           knock_in_price=5.2,
-                           notional=1000000,
-                           rebate=0.2,
-                           knock_in_type='Return',
-                           value_date=TuringDate(2021, 2, 3),
-                           multiplier=100,
-                           stock_price=5.262,
-                           volatility=0.1,
-                           interest_rate=0.02,
-                           dividend_yield=0)
-
-knockout_option = EqOption(option_type='CALL',
-                           product_type='KNOCK_OUT',
-                           knock_out_type='up_and_out',
-                           expiration_date=TuringDate(2021, 9, 3),
-                           strike_price=5.3,
-                           participation_rate=1.0,
-                           barrier=5.5,
-                           notional=1000000,
-                           rebate=0.2,
-                           value_date=TuringDate(2021, 2, 3),
-                           multiplier=100,
-                           stock_price=5.262,
-                           volatility=0.1,
-                           interest_rate=0.02,
-                           dividend_yield=0)
-
-
+# from turing_models.instrument.eq_option import EqOption
+#
+#
+european_option = EuropeanOption(option_type='CALL',
+                                 underlier='0',
+                                 expiration_date=TuringDate(2021, 9, 3),
+                                 strike_price=5.3,
+                                 number_of_options=2,
+                                 multiplier=100,
+                                 value_date=TuringDate(2021, 2, 3),
+                                 stock_price=5.262,
+                                 volatility=0.1,
+                                 interest_rate=0.02,
+                                 dividend_yield=0)
+#
+#
+# american_option = EqOption(option_type='CALL',
+#                            product_type='AMERICAN',
+#                            expiration_date=TuringDate(2021, 9, 3),
+#                            strike_price=5.3,
+#                            number_of_options=2,
+#                            multiplier=100,
+#                            value_date=TuringDate(2021, 2, 3),
+#                            stock_price=5.262,
+#                            volatility=0.1,
+#                            interest_rate=0.02,
+#                            dividend_yield=0)
+#
+# asian_option = EqOption(option_type='CALL',
+#                         product_type='ASIAN',
+#                         expiration_date=TuringDate(2021, 9, 3),
+#                         start_averaging_date=TuringDate(2021, 2, 15),
+#                         strike_price=5.3,
+#                         number_of_options=2,
+#                         value_date=TuringDate(2021, 2, 3),
+#                         multiplier=100,
+#                         stock_price=5.262,
+#                         volatility=0.1,
+#                         interest_rate=0.02,
+#                         dividend_yield=0)
+#
+# snowball_option = EqOption(option_type='CALL',
+#                            product_type='SNOWBALL',
+#                            expiration_date=TuringDate(2021, 9, 3),
+#                            participation_rate=1.0,
+#                            barrier=5.5,
+#                            knock_in_price=5.2,
+#                            notional=1000000,
+#                            rebate=0.2,
+#                            knock_in_type='Return',
+#                            value_date=TuringDate(2021, 2, 3),
+#                            multiplier=100,
+#                            stock_price=5.262,
+#                            volatility=0.1,
+#                            interest_rate=0.02,
+#                            dividend_yield=0)
+#
+# knockout_option = EqOption(option_type='CALL',
+#                            product_type='KNOCK_OUT',
+#                            knock_out_type='up_and_out',
+#                            expiration_date=TuringDate(2021, 9, 3),
+#                            strike_price=5.3,
+#                            participation_rate=1.0,
+#                            barrier=5.5,
+#                            notional=1000000,
+#                            rebate=0.2,
+#                            value_date=TuringDate(2021, 2, 3),
+#                            multiplier=100,
+#                            stock_price=5.262,
+#                            volatility=0.1,
+#                            interest_rate=0.02,
+#                            dividend_yield=0)
+#
+#
 print_result(european_option)
-print_result(american_option)
-print_result(asian_option)
-print_result(snowball_option)
-print_result(knockout_option)
-
-
+# print_result(american_option)
+# print_result(asian_option)
+# print_result(snowball_option)
+# print_result(knockout_option)
+#
+#
 with PricingContext(interest_rate=0.04):
     print_result(european_option)
-    print_result(american_option)
-    print_result(asian_option)
-    print_result(snowball_option)
-    print_result(knockout_option)
-
-
+#     print_result(american_option)
+#     print_result(asian_option)
+#     print_result(snowball_option)
+#     print_result(knockout_option)
+#
+#
 with PricingContext(interest_rate=0.04):
     print_result(european_option)
-    print_result(american_option)
-    print_result(asian_option)
-    print_result(snowball_option)
-    print_result(knockout_option)
-
-
+#     print_result(american_option)
+#     print_result(asian_option)
+#     print_result(snowball_option)
+#     print_result(knockout_option)
+#
+#
 with PricingContext(interest_rate=0.06):
     print_result(european_option)
-    print_result(american_option)
-    print_result(asian_option)
-    print_result(snowball_option)
-    print_result(knockout_option)
-
+#     print_result(american_option)
+#     print_result(asian_option)
+#     print_result(snowball_option)
+#     print_result(knockout_option)
+#
---
+++ turing_models/instrument/european_option.py
@@ -0,0 +1,180 @@
+import datetime
+from dataclasses import dataclass
+
+import numpy as np
+from tunny import model
+from fundamental.base import Context, ctx
+from fundamental.market.curves import TuringDiscountCurveFlat
+
+from turing_models.utilities.turing_date import TuringDate
+from turing_models.utilities.global_variables import gDaysInYear
+from turing_models.utilities.global_types import TuringOptionTypes
+from turing_models.models.model_black_scholes import TuringModelBlackScholes
+from turing_models.models.model_black_scholes_analytical import bsValue, bsDelta, \
+     bsVega, bsGamma, bsRho, bsPsi, bsTheta
+
+
+@model
+@dataclass
+class EuropeanOption:
+    """
+        Instrument definition for equity option
+        支持多种参数传入方式
+        Examples:
+        1.
+        # >>> eq = EqOption(asset_id='123', option_type='CALL', product_type='European', expiration_date=TuringDate(2021, 2, 12), strike_price=90, multiplier=10000)
+        # >>> eq.from_json()
+        # >>> eq.price()
+        2.
+        # >>> _option = Option()
+        # >>> _option.resolve(_resource=somedict)
+        # >>> eq = EqOption(obj=_option)
+        # >>> eq.resolve()
+        # >>> eq.price()
+        3.
+        # >>> _option = Option()
+        # >>> _option.resolve(_resource=somedict)
+        # >>> eq = EqOption(option_type='CALL',product_type='European', notional=1.00, obj=_option)
+        # >>> eq.resolve()
+        # >>> eq.price()
+    """
+
+    asset_id: str = None
+    option_type: str = None
+    underlier: str = None
+    notional: float = None
+    initial_spot: float = None
+    number_of_options: float = None
+    start_date: str = None
+    end_date: str = None
+    start_averaging_date: str = None
+    expiration_date: str = None
+    participation_rate: float = None
+    strike_price: float = None
+    barrier: float = None
+    rebate: float = None
+    coupon: float = None
+    multiplier: float = None
+    currency: str = None
+    premium: float = None
+    premium_date: str = None
+    knock_in_price: float = None
+    coupon_annualized_flag: bool = True
+    knock_out_type: str = None
+    knock_in_type: str = None
+    knock_in_strike1: float = None
+    knock_in_strike2: float = None
+    accrued_average: float = None
+    value_date: TuringDate = TuringDate(*(datetime.date.today().timetuple()[:3]))  # 估值日期
+    stock_price: float = None
+    volatility: float = 0.1
+    interest_rate: float = 0.02
+    dividend_yield: float = 0
+    ctx: Context = ctx
+
+    def __post_init__(self):
+        self.ctx = ctx
+        self.set_param()
+
+    def set_param(self):
+        self._value_date = self.value_date
+        self._stock_price = self.stock_price
+        self._volatility = self.volatility
+        self._interest_rate = self.interest_rate
+        self._dividend_yield = self.dividend_yield
+
+    def _set_by_dict(self, tmp_dict):
+        for k, v in tmp_dict.items():
+            setattr(self, k, v)
+
+    def resolve(self, expand_dict):
+        self._set_by_dict(expand_dict)
+        self.set_param()
+
+    @property
+    def value_date_(self):
+        return self.ctx.pricing_date or self._value_date
+
+    @property
+    def stock_price_(self) -> float:
+        return getattr(self.ctx, f"spot_{self.underlier}") or self._stock_price
+
+    @property
+    def volatility_(self) -> float:
+        return getattr(self.ctx, f"volatility_{self.underlier}") or self._volatility
+
+    @property
+    def interest_rate_(self) -> float:
+        return self.ctx.interest_rate or self.interest_rate
+
+    @property
+    def dividend_yield_(self) -> float:
+        return self.ctx.dividend_yield or self._dividend_yield
+
+    @property
+    def model(self) -> TuringModelBlackScholes:
+        return TuringModelBlackScholes(self.volatility_)
+
+    @property
+    def discount_curve(self) -> TuringDiscountCurveFlat:
+        return TuringDiscountCurveFlat(
+            self.value_date_, self.interest_rate_)
+
+    @property
+    def dividend_curve(self) -> TuringDiscountCurveFlat:
+        return TuringDiscountCurveFlat(
+            self.value_date_, self.dividend_yield_)
+
+    @property
+    def texp(self) -> float:
+        return (self.expiration_date - self.value_date_) / gDaysInYear
+
+    @property
+    def r(self) -> float:
+        df = self.discount_curve.df(self.expiration_date)
+        return -np.log(df)/self.texp
+
+    @property
+    def q(self) -> float:
+        dq = self.dividend_curve.df(self.expiration_date)
+        return -np.log(dq)/self.texp
+
+    @property
+    def v(self) -> float:
+        return self.model._volatility
+
+    @property
+    def option_type_(self) -> TuringOptionTypes:
+        return TuringOptionTypes.EUROPEAN_CALL if self.option_type == 'CALL' else TuringOptionTypes.EUROPEAN_PUT
+
+    def params(self) -> list:
+        return [
+            self.stock_price_,
+            self.texp,
+            self.strike_price,
+            self.r,
+            self.q,
+            self.v,
+            self.option_type_.value
+        ]
+
+    def price(self) -> float:
+        return bsValue(*self.params())
+
+    def eq_delta(self) -> float:
+        return bsDelta(*self.params())
+
+    def eq_gamma(self) -> float:
+        return bsGamma(*self.params())
+
+    def eq_vega(self) -> float:
+        return bsVega(*self.params())
+
+    def eq_theta(self) -> float:
+        return bsTheta(*self.params())
+
+    def eq_rho(self) -> float:
+        return bsRho(*self.params())
+
+    def eq_rho_q(self) -> float:
+        return bsPsi(*self.params())
