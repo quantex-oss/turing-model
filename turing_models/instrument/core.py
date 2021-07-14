f5449b6147ef652b73df1cb6054f0e6828dab5fd
杨晓亮 <xiaoliang.yang>
2021-07-14 10:28:44 +0800

    no message

--- turing_models/instrument/core.py
+++ turing_models/instrument/core.py
@@ -1,21 +1,79 @@
 import traceback
-from typing import Union, List, Iterable
+from dataclasses import dataclass, field
+from typing import Union, List, Iterable, Any
+
 
 from loguru import logger
 
 from fundamental import ctx
 from fundamental.base import Context
 from turing_models.instrument.common import RiskMeasure
+from turing_models.instrument.decorator import concurrent
+
+package_ref = 'sn:cn:yrk:12345678901234561234567890123456:function:0-turing-model:$latest'
 
 
-# from turing_models.instrument.decorator import concurrent
+@dataclass
+class YuanRong:
+    obj: Any = None
+    time_out: int = 10
+
+    def __post_init__(self):
+        self.greeks = [
+            RiskMeasure.Price, RiskMeasure.EqDelta,
+            RiskMeasure.EqGamma, RiskMeasure.EqVega,
+            RiskMeasure.EqTheta, RiskMeasure.EqRho
+        ]
+
+
+    def build_obj_list(self):
+        if not isinstance(self.obj, list):
+            self.obj = [self.obj]
+        obj_ = [self.calc_(o) for o in self.obj]
+        return [n for a in obj_ for n in a]
+
+    def calc_(self, obj):
+        return [obj.yuanrong_calc(x) for x in self.greeks]
+
+    def __call__(self, *args, **kwargs):
+        import yuanrong
+        self.obj_list = self.build_obj_list()
+        if isinstance(self.obj_list, list):
+            try:
+                return yuanrong.get(self.obj_list, self.time_out)
+            except RuntimeError:
+                self.obj_list = self.build_obj_list()
+                return yuanrong.get(self.obj_list, self.time_out)
+            except Exception as e:
+                traceback.format_exc()
+                return []
+
+    @staticmethod
+    def init():
+        import yuanrong
+        yuanrong.init(
+            package_ref=package_ref,
+            logging_level='INFO',
+            cluster_server_addr='123.60.60.83'
+        )
 
 
 class Instrument:
     def __init__(self):
         self.ctx: Context = ctx
 
-    # @concurrent
+    @concurrent
+    def yuanrong_calc(self, risk_measure: Union[RiskMeasure, List[RiskMeasure]]):
+        result: Union[float, List] = []
+        name: list = []
+        try:
+            name = [getattr(self, "asset_id", None), risk_measure.value]
+            result = getattr(self, risk_measure.value)()
+            return name, result
+        except Exception as e:
+            logger.error(str(traceback.format_exc()))
+            return name, ""
+
     def calc(self, risk_measure: Union[RiskMeasure, List[RiskMeasure]]):
         result: Union[float, List] = []
 
@@ -23,13 +81,15 @@ class Instrument:
             if not isinstance(risk_measure, Iterable):
                 result = getattr(self, risk_measure.value)()
                 result = self._calc(result)
-                self.__row__(risk_measure.value, round(result, 2) if result else "-")
+                self.__row__(risk_measure.value, round(result, 2)
+                if result else "-")
                 return result
             for risk in risk_measure:
                 res = getattr(self, risk.value)()
                 res = self._calc(res)
                 result.append(res)
-                self.__row__(risk.value, round(res, 2) if res and not isinstance(res, Iterable) else "-")
+                self.__row__(risk.value, round(res, 2)
+                if res and not isinstance(res, Iterable) else "-")
             return result
         except Exception as e:
             logger.error(str(traceback.format_exc()))
