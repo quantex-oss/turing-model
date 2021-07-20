f2c71e2d2acd32e1933656bfb80554ae8c1eb8b5
wangyuting <yuting.wang@iquantex.com>
2021-07-20 17:13:49 +0800

    修改TuringFrequencyTypes

--- turing_models/utilities/frequency.py
+++ turing_models/utilities/frequency.py
@@ -13,6 +13,7 @@ class TuringFrequencyTypes(Enum):
     QUARTERLY = 4
     MONTHLY = 12
     CONTINUOUS = 99
+    FORTNIGHTLY = 26
     WEEKLY = 52
     DAILY = 365
 
@@ -40,6 +41,8 @@ def TuringFrequency(freqType):
         return 4
     elif freqType == TuringFrequencyTypes.MONTHLY:
         return 12
+    elif freqType == TuringFrequencyTypes.FORTNIGHTLY:
+        return 26
     elif freqType == TuringFrequencyTypes.WEEKLY:
         return 52
     elif freqType == TuringFrequencyTypes.DAILY:
