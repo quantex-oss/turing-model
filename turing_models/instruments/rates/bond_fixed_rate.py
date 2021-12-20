6e710e0c1981a3e6bf384eb11312a0cee92dcd38
yan.zhuang <yan.zhuang@iquantex.com>
2021-12-20 11:28:11 +0800

    consider zero and discount bond, add TTM

--- turing_models/instruments/rates/bond_fixed_rate.py
+++ turing_models/instruments/rates/bond_fixed_rate.py
@@ -9,7 +9,8 @@ from turing_models.market.curves.discount_curve import TuringDiscountCurve
 from turing_models.utilities.calendar import TuringCalendar
 from turing_models.utilities.day_count import TuringDayCount, TuringDayCountTypes
 from turing_models.utilities.error import TuringError
-from turing_models.utilities.global_types import TuringYTMCalcType
+from turing_models.utilities.frequency import TuringFrequencyTypes
+from turing_models.utilities.global_types import TuringCouponType, TuringYTMCalcType
 from turing_models.utilities.helper_functions import to_string
 from turing_models.market.curves.curve_adjust import CurveAdjustmentImpl
 from turing_models.market.curves.discount_curve_flat import TuringDiscountCurveFlat
@@ -87,7 +88,12 @@ class BondFixedRate(Bond):
         self._flow_amounts = [0.0]
 
         for _ in self._flow_dates[1:]:
-            cpn = self.coupon / self.frequency
+            if self.cpn_type == TuringCouponType.DISCOUNT:
+                cpn = 0
+            elif self.cpn_type == TuringCouponType.ZERO_COUPON:
+                cpn = self.coupon * self.bond_term_year
+            elif self.cpn_type == TuringCouponType.COUPON_CARRYING:
+                cpn = self.coupon / self.frequency
             self._flow_amounts.append(cpn)
 
     def implied_spread(self):
@@ -125,8 +131,6 @@ class BondFixedRate(Bond):
                                                 parallel_shift=self.spread_adjustment,
                                                 value_date=self.settlement_date_).get_curve_result()
 
-        discount_curve_flat = TuringDiscountCurveFlat(self.settlement_date_, self.ytm_)
-
         px = 0.0
         df = 1.0
         df_settle = self.curve_fitted.df(self.settlement_date_)
@@ -145,10 +149,7 @@ class BondFixedRate(Bond):
         px += df * self._redemption * self.par * dates
         px = px / df_settle
 
-        self.discount_curve = discount_curve_flat
-        fp = self.full_price_from_discount_curve()
-        self.discount_curve = None
-
+        fp = self.full_price_from_ytm()
         dmac = px / fp
 
         return dmac
@@ -173,55 +174,63 @@ class BondFixedRate(Bond):
 
         ytm = np.array(self.ytm_)  # 向量化
         ytm = ytm + 0.000000000012345  # 防止ytm = 0
+        if self.cpn_type == TuringCouponType.COUPON_CARRYING:
+            f = self.frequency
+            c = self.coupon
+            v = 1.0 / (1.0 + ytm / f)
 
-        f = self.frequency
-        c = self.coupon
-        v = 1.0 / (1.0 + ytm / f)
+            # n是下一付息日后的现金流个数
+            n = 0
+            for dt in self._flow_dates:
+                if dt > self.settlement_date_:
+                    n += 1
+            n = n - 1
 
-        # n是下一付息日后的现金流个数
-        n = 0
-        for dt in self._flow_dates:
-            if dt > self.settlement_date_:
-                n += 1
-        n = n - 1
+            if n < 0:
+                raise TuringError("No coupons left")
 
-        if n < 0:
-            raise TuringError("No coupons left")
-
-        if self.convention == TuringYTMCalcType.UK_DMO:
-            if n == 0:
-                fp = (v ** (self._alpha)) * (self._redemption + c / f)
-            else:
-                term1 = (c / f)
-                term2 = (c / f) * v
-                term3 = (c / f) * v * v * (1.0 - v ** (n - 1)) / (1.0 - v)
-                term4 = self._redemption * (v ** n)
-                fp = (v ** (self._alpha)) * (term1 + term2 + term3 + term4)
-        elif self.convention == TuringYTMCalcType.US_TREASURY:
-            if n == 0:
-                fp = (v ** (self._alpha)) * (self._redemption + c / f)
-            else:
-                term1 = (c / f)
-                term2 = (c / f) * v
-                term3 = (c / f) * v * v * (1.0 - v ** (n - 1)) / (1.0 - v)
-                term4 = self._redemption * (v ** n)
+            if self.convention == TuringYTMCalcType.UK_DMO:
+                if n == 0:
+                    fp = (v ** (self._alpha)) * (self._redemption + c / f)
+                else:
+                    term1 = (c / f)
+                    term2 = (c / f) * v
+                    term3 = (c / f) * v * v * (1.0 - v ** (n - 1)) / (1.0 - v)
+                    term4 = self._redemption * (v ** n)
+                    fp = (v ** (self._alpha)) * (term1 + term2 + term3 + term4)
+            elif self.convention == TuringYTMCalcType.US_TREASURY:
+                if n == 0:
+                    fp = (v ** (self._alpha)) * (self._redemption + c / f)
+                else:
+                    term1 = (c / f)
+                    term2 = (c / f) * v
+                    term3 = (c / f) * v * v * (1.0 - v ** (n - 1)) / (1.0 - v)
+                    term4 = self._redemption * (v ** n)
+                    vw = 1.0 / (1.0 + self._alpha * ytm / f)
+                    fp = (vw) * (term1 + term2 + term3 + term4)
+            elif self.convention == TuringYTMCalcType.US_STREET:
                 vw = 1.0 / (1.0 + self._alpha * ytm / f)
-                fp = (vw) * (term1 + term2 + term3 + term4)
-        elif self.convention == TuringYTMCalcType.US_STREET:
-            vw = 1.0 / (1.0 + self._alpha * ytm / f)
-            if n == 0:
-                vw = 1.0 / (1.0 + self._alpha * ytm / f)
-                fp = vw * (self._redemption + c / f)
+                if n == 0:
+                    vw = 1.0 / (1.0 + self._alpha * ytm / f)
+                    fp = vw * (self._redemption + c / f)
+                else:
+                    term1 = (c / f)
+                    term2 = (c / f) * v
+                    term3 = (c / f) * v * v * (1.0 - v ** (n - 1)) / (1.0 - v)
+                    term4 = self._redemption * (v ** n)
+                    fp = (v ** (self._alpha)) * (term1 + term2 + term3 + term4)
             else:
-                term1 = (c / f)
-                term2 = (c / f) * v
-                term3 = (c / f) * v * v * (1.0 - v ** (n - 1)) / (1.0 - v)
-                term4 = self._redemption * (v ** n)
-                fp = (v ** (self._alpha)) * (term1 + term2 + term3 + term4)
+                raise TuringError("Unknown yield convention")
+
+            return fp * self.par
+        
         else:
-            raise TuringError("Unknown yield convention")
-
-        return fp * self.par
+            if self.time_to_maturity_in_year <= 1:
+                fp = (self._redemption + self._flow_amounts[-1]) * self.par / (ytm * self.time_to_maturity_in_year + 1)
+            else:
+                fp = ((self._redemption + self._flow_amounts[-1]) * self.par / (ytm + 1)) ** self.time_to_maturity_in_year
+            return fp
+                
 
     def clean_price_from_ytm(self):
         """ 通过YTM计算净价 """
@@ -248,9 +257,14 @@ class BondFixedRate(Bond):
             # 将结算日的票息加入计算
             if dt >= self.settlement_date_:
                 df = self.curve_fitted.df(dt)
-                flow = self.coupon / self.frequency
-                pv = flow * df
-                px += pv
+                if self.cpn_type == TuringCouponType.COUPON_CARRYING:
+                    flow = self.coupon / self.frequency
+                    pv = flow * df
+                    px += pv
+                else:
+                    flow = self._flow_amounts[-1]
+                    pv = flow * df
+                    px += pv
 
         px += df * self._redemption
         px = px / df_settle
@@ -327,16 +341,24 @@ class BondFixedRate(Bond):
         cal = TuringCalendar(self.calendar_type)
         ex_dividend_date = cal.addBusinessDays(
             self._ncd, -self.num_ex_dividend_days)  # 除息日
+        
+        if self.cpn_type == TuringCouponType.COUPON_CARRYING:
+            (acc_factor, num, _) = dc.yearFrac(self._pcd,
+                                            self.settlement_date_,
+                                            self._ncd,
+                                            self.freq_type)  # 计算应计日期，返回应计因数、应计天数、基数
+            
+            if self.settlement_date_ > ex_dividend_date:  # 如果结算日大于除息日，减去一期
+                acc_factor = acc_factor - 1.0 / self.frequency
+            
+            self._alpha = 1.0 - acc_factor * self.frequency  # 计算alpha值供全价计算公式使用
+                
+            
+        else:
+            (acc_factor, num, _) = dc.yearFrac(self._pcd,
+                                           self.settlement_date_)
 
-        (acc_factor, num, _) = dc.yearFrac(self._pcd,
-                                           self.settlement_date_,
-                                           self._ncd,
-                                           self.freq_type)  # 计算应计日期，返回应计因数、应计天数、基数
-
-        if self.settlement_date_ > ex_dividend_date:  # 如果结算日大于除息日，减去一期
-            acc_factor = acc_factor - 1.0 / self.frequency
-
-        self._alpha = 1.0 - acc_factor * self.frequency  # 计算alpha值供全价计算公式使用
+        
         self._accrued_interest = acc_factor * self.par * self.coupon
         self._accrued_days = num
 
