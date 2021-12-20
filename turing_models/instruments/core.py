7889da07d3bc1d789f2834c8e86daa28a1838aa3
yuting.wang <yuting.wang@iquantex.com>
2021-12-20 16:35:39 +0800

    添加quanto相关的定价服务api参数文档

--- turing_models/instruments/core.py
+++ turing_models/instruments/core.py
@@ -4,7 +4,7 @@ from typing import Union, List, Iterable
 
 from fundamental import PricingContext
 from fundamental.base import ctx, Context
-from utils.log.request_id_log import logger
+from utility.log.request_id_log import logger
 from turing_models.instruments.common import RiskMeasure
 
 
