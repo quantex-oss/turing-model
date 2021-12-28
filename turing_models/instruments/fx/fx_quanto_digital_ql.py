115195edf125047be4397e6b8235c54af8092ad9
yuting.wang <yuting.wang@iquantex.com>
2021-12-28 18:21:07 +0800

    reformat code

--- turing_models/instruments/fx/fx_quanto_digital_ql.py
+++ turing_models/instruments/fx/fx_quanto_digital_ql.py
@@ -6,14 +6,14 @@ Created on Tue Sep 14 14:12:19 2021
 """
 
 import math
-import numpy as np
-import pandas as pd
-import QuantLib as ql
 from scipy.stats import norm
+
+from turing_models.utilities.error import TuringError
 
 
 class FXQuantoDigitalQL:
-    def __init__(self, f_ccy, d_ccy, q_ccy, strike, accrualStart, accrualEnd, obs_start, obs_end, flavor, notional, coupon_rate, tradeDirection):
+    def __init__(self, f_ccy, d_ccy, q_ccy, strike, accrualStart, accrualEnd, obs_start, obs_end, flavor, notional,
+                 coupon_rate, tradeDirection):
         self.f_ccy = f_ccy
         self.d_ccy = d_ccy
         self.q_ccy = q_ccy
@@ -27,7 +27,7 @@ class FXQuantoDigitalQL:
         self.coupon_rate = coupon_rate
         self.tradeDirection = tradeDirection.lower()
 
- # quanto-adjusted ATM
+    # quanto-adjusted ATM
     def __getATM__(self, today, spot_f_d, fwd_crv_fd,
                    sigma_f_d, sigma_d_q, rho_fd_dq, daycount):
 
@@ -38,9 +38,9 @@ class FXQuantoDigitalQL:
 
         return atm_adj
 
-# NPV
+    # NPV
     def NPV(self, today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, vtk_f_d = 0.0, vtk_d_q = 0.0):
+            sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, vtk_f_d=0.0, vtk_d_q=0.0):
 
         # sigma_f_d is the volatility surface of f/d, given an expiry date and strike price, we can get a volatility, then add a tweak
         sigma_f_d = sigma_f_d.interp_vol(self.obs_end, self.strike) + vtk_f_d
@@ -51,26 +51,31 @@ class FXQuantoDigitalQL:
 
         dcf = daycount.yearFraction(today, self.obs_end)
         atm = self.__getATM__(today, spot_f_d, fwd_crv_fd,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, daycount)
+                              sigma_f_d, sigma_d_q, rho_fd_dq, daycount)
 
         d2 = (math.log(atm / self.strike) - 0.5 * sigma_f_d ** 2 * dcf) / (sigma_f_d * math.sqrt(dcf))
         df = q_crv.discount(self.accrualEnd) / q_crv.discount(today)
-
 
         coupon_dcf = daycount.yearFraction(self.accrualStart, self.accrualEnd)
 
         if self.flavor == 'call':
             npv = df * (norm.cdf(d2)) * self.notional * self.coupon_rate * coupon_dcf
 
-        else:
+        elif self.flavor == 'put':
             npv = df * (norm.cdf(-d2)) * self.notional * self.coupon_rate * coupon_dcf
 
-        return npv if self.tradeDirection == 'long' else -npv
+        else:
+            raise TuringError('Please check the input of flavor')
 
-
+        if self.tradeDirection == 'long':
+            return npv
+        elif self.tradeDirection == 'short':
+            return -npv
+        else:
+            raise TuringError('Please check the input of tradeDirection')
 
     def Exercise_Prob(self, today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, vtk_f_d = 0.0, vtk_d_q = 0.0):
+                      sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, vtk_f_d=0.0, vtk_d_q=0.0):
         # sigma_f_d is the volatility surface of f/d, given an expiry date and strike price, we can get a volatility, then add a tweak
         sigma_f_d = sigma_f_d.interp_vol(self.obs_end, self.strike) + vtk_f_d
 
@@ -80,101 +85,87 @@ class FXQuantoDigitalQL:
 
         dcf = daycount.yearFraction(today, self.obs_end)
         atm = self.__getATM__(today, spot_f_d, fwd_crv_fd,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, daycount)
+                              sigma_f_d, sigma_d_q, rho_fd_dq, daycount)
 
         d2 = (math.log(atm / self.strike) - 0.5 * sigma_f_d ** 2 * dcf) / (sigma_f_d * math.sqrt(dcf))
 
         if self.flavor == 'call':
             return norm.cdf(d2)
 
-        else:
+        elif self.flavor == 'put':
             return norm.cdf(-d2)
 
+        else:
+            raise TuringError('Please check the input of flavor')
 
-
-#Delta wrt Foreign/Domestic FX spot
+    # Delta wrt Foreign/Domestic FX spot
     def Delta(self, today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak = 1):
+              sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak=1):
 
+        npv_up = self.NPV(today, spot_f_d + tweak / 10000, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
+                          sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention)
 
-        npv_up = self.NPV(today, spot_f_d + tweak/10000, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention)
-
-
-        npv_down = self.NPV(today, spot_f_d - tweak/10000, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention)
+        npv_down = self.NPV(today, spot_f_d - tweak / 10000, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
+                            sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention)
 
         delta = (npv_up - npv_down) / (2 * tweak)
 
-        res_delta = delta*10000 /spot_d_q
+        res_delta = delta * 10000 / spot_d_q
 
         return res_delta
 
-
-#Gamma wrt Foreign/Domestic FX spot
+    # Gamma wrt Foreign/Domestic FX spot
     def Gamma(self, today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak = 1):
+              sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak=1):
 
+        delta_up = self.Delta(today, spot_f_d + tweak / 10000, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
+                              sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak)
 
-        delta_up = self.Delta(today, spot_f_d + tweak/10000, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak)
+        delta_down = self.Delta(today, spot_f_d - tweak / 10000, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
+                                sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak)
 
-
-        delta_down = self.Delta(today, spot_f_d - tweak/10000, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak)
-
-
-        gamma = (delta_up - delta_down)/(2*tweak)
+        gamma = (delta_up - delta_down) / (2 * tweak)
 
         return gamma
 
-
-#Vega wrt Foreign/Domestic FX Vol
+    # Vega wrt Foreign/Domestic FX Vol
     def Vega(self, today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak =1):
-
+             sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak=1):
 
         npv_up = self.NPV(today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak/100)
-
+                          sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak / 100)
 
         npv_down = self.NPV(today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, -tweak/100)
+                            sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, -tweak / 100)
 
         vega = (npv_up - npv_down) / (2 * tweak)
 
         return vega
 
-
-#Vega wrt Domestic/Quanto FX Vol
+    # Vega wrt Domestic/Quanto FX Vol
 
     def QuantoVega(self, today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak =1):
-
+                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak=1):
 
         npv_up = self.NPV(today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, 0, tweak/100)
-
+                          sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, 0, tweak / 100)
 
         npv_down = self.NPV(today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, 0, -tweak/100)
+                            sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, 0, -tweak / 100)
 
         quantovega = (npv_up - npv_down) / (2 * tweak)
 
         return quantovega
 
-
-#sensitivity wrt to corr input
+    # sensitivity wrt to corr input
     def Corr_sens(self, today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak=1):
-
+                  sigma_f_d, sigma_d_q, rho_fd_dq, calendar, daycount, convention, tweak=1):
 
         npv_up = self.NPV(today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq + tweak/100, calendar, daycount, convention)
-
+                          sigma_f_d, sigma_d_q, rho_fd_dq + tweak / 100, calendar, daycount, convention)
 
         npv_down = self.NPV(today, spot_f_d, spot_d_q, fwd_crv_fd, fwd_crv_dq, q_crv,
-                   sigma_f_d, sigma_d_q, rho_fd_dq - tweak/100, calendar, daycount, convention)
+                            sigma_f_d, sigma_d_q, rho_fd_dq - tweak / 100, calendar, daycount, convention)
 
         corr_sens = (npv_up - npv_down) / (2 * tweak)
 
