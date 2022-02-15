8d3e0d433ec36a7dc42308518aa38b918f1879c4
tong.zhang <tong.zhang@iquantex.com>
2022-02-15 14:09:02 +0800

    添加BasketSnowballOption测试用例

--- demo_tests/eqoption_test.py
+++ demo_tests/eqoption_test.py
@@ -20,67 +20,67 @@ from turing_models.utilities.global_types import OptionType
 from turing_models.instruments.common import Currency, RiskMeasure
 from turing_models.utilities.helper_functions import betaVectorToCorrMatrix
 
-european_option = EuropeanOption(underlier_symbol='600067.SH',
-                                 option_type=OptionType.CALL,
-                                 expiry=datetime.datetime(2021, 9, 3),
-                                 start_date=datetime.datetime(2021, 6, 3),
-                                 strike_price=4.19,
-                                 number_of_options=2,
-                                 multiplier=100,
-                                 value_date=datetime.datetime(2021, 8, 13),
-                                 currency=Currency.CNY)
+# european_option = EuropeanOption(underlier_symbol='600067.SH',
+#                                  option_type=OptionType.CALL,
+#                                  expiry=datetime.datetime(2021, 9, 3),
+#                                  start_date=datetime.datetime(2021, 6, 3),
+#                                  strike_price=4.19,
+#                                  number_of_options=2,
+#                                  multiplier=100,
+#                                  value_date=datetime.datetime(2021, 8, 13),
+#                                  currency=Currency.CNY)
 
-american_option = AmericanOption(underlier_symbol='600067.SH',
-                                 option_type=OptionType.CALL,
-                                 expiry=datetime.datetime(2021, 9, 3),
-                                 start_date=datetime.datetime(2021, 6, 3),
-                                 strike_price=4.19,
-                                 number_of_options=2,
-                                 multiplier=100,
-                                 value_date=datetime.datetime(2021, 8, 13),
-                                 currency=Currency.CNY)
+# american_option = AmericanOption(underlier_symbol='600067.SH',
+#                                  option_type=OptionType.CALL,
+#                                  expiry=datetime.datetime(2021, 9, 3),
+#                                  start_date=datetime.datetime(2021, 6, 3),
+#                                  strike_price=4.19,
+#                                  number_of_options=2,
+#                                  multiplier=100,
+#                                  value_date=datetime.datetime(2021, 8, 13),
+#                                  currency=Currency.CNY)
 
-asian_option = AsianOption(underlier_symbol='600067.SH',
-                           option_type=OptionType.CALL,
-                           expiry=datetime.datetime(2021, 9, 3),
-                           start_date=datetime.datetime(2021, 6, 3),
-                           start_averaging_date=datetime.datetime(2021, 8, 15),
-                           strike_price=4.19,
-                           number_of_options=2,
-                           value_date=datetime.datetime(2021, 8, 13),
-                           currency=Currency.CNY,
-                           multiplier=100)
+# asian_option = AsianOption(underlier_symbol='600067.SH',
+#                            option_type=OptionType.CALL,
+#                            expiry=datetime.datetime(2021, 9, 3),
+#                            start_date=datetime.datetime(2021, 6, 3),
+#                            start_averaging_date=datetime.datetime(2021, 8, 15),
+#                            strike_price=4.19,
+#                            number_of_options=2,
+#                            value_date=datetime.datetime(2021, 8, 13),
+#                            currency=Currency.CNY,
+#                            multiplier=100)
 
-snowball_option = SnowballOption(underlier_symbol='600067.SH',
-                                 option_type=OptionType.CALL,
-                                 start_date=datetime.datetime(2021, 6, 3),
-                                 expiry=datetime.datetime(2021, 10, 3),
-                                 participation_rate=1.0,
-                                 barrier=4.25,
-                                 knock_in_price=4.1,
-                                 notional=1000000,
-                                 rebate=0.2,
-                                 initial_spot=4.2,
-                                 untriggered_rebate=0.2,
-                                 knock_in_type='SPREADS',
-                                 knock_out_obs_days_whole=[datetime.datetime(2021, 6, 3), datetime.datetime(
-                                     2021, 7, 5), datetime.datetime(2021, 8, 3), datetime.datetime(2021, 9, 3)],
-                                 knock_in_strike1=4.23,
-                                 knock_in_strike2=4.24,
-                                 value_date=datetime.datetime(2021, 8, 13),
-                                 currency=Currency.CNY)
+# snowball_option = SnowballOption(underlier_symbol='600067.SH',
+#                                  option_type=OptionType.CALL,
+#                                  start_date=datetime.datetime(2021, 6, 3),
+#                                  expiry=datetime.datetime(2021, 10, 3),
+#                                  participation_rate=1.0,
+#                                  barrier=4.25,
+#                                  knock_in_price=4.1,
+#                                  notional=1000000,
+#                                  rebate=0.2,
+#                                  initial_spot=4.2,
+#                                  untriggered_rebate=0.2,
+#                                  knock_in_type='SPREADS',
+#                                  knock_out_obs_days_whole=[datetime.datetime(2021, 6, 3), datetime.datetime(
+#                                      2021, 7, 5), datetime.datetime(2021, 8, 3), datetime.datetime(2021, 9, 3)],
+#                                  knock_in_strike1=4.23,
+#                                  knock_in_strike2=4.24,
+#                                  value_date=datetime.datetime(2021, 8, 13),
+#                                  currency=Currency.CNY)
 
-knockout_option = KnockOutOption(underlier_symbol='600067.SH',
-                                 option_type=OptionType.CALL,
-                                 start_date=datetime.datetime(2021, 6, 3),
-                                 expiry=datetime.datetime(2021, 9, 3),
-                                 strike_price=4.19,
-                                 participation_rate=1.0,
-                                 barrier=5.5,
-                                 notional=1000000,
-                                 rebate=0.2,
-                                 value_date=datetime.datetime(2021, 8, 13),
-                                 currency=Currency.CNY)
+# knockout_option = KnockOutOption(underlier_symbol='600067.SH',
+#                                  option_type=OptionType.CALL,
+#                                  start_date=datetime.datetime(2021, 6, 3),
+#                                  expiry=datetime.datetime(2021, 9, 3),
+#                                  strike_price=4.19,
+#                                  participation_rate=1.0,
+#                                  barrier=5.5,
+#                                  notional=1000000,
+#                                  rebate=0.2,
+#                                  value_date=datetime.datetime(2021, 8, 13),
+#                                  currency=Currency.CNY)
 
 # def test_european_option_original():
 #     assert round(european_option.calc(RiskMeasure.Price), 3) == 2.156
@@ -126,20 +126,24 @@ knockout_option = KnockOutOption(underlier_symbol='600067.SH',
 #     assert round(knockout_option.calc(RiskMeasure.EqTheta), 3) == -26640.928
 #     assert round(knockout_option.calc(RiskMeasure.EqRho), 3) == 32781.571
 #     assert round(knockout_option.calc(RiskMeasure.EqRhoQ), 3) == -32935.022
-  
+
+# def basket_snowball_option_original():
+#     assert round(basket_snowball_option.calc(RiskMeasure.Price), 3) == 0.0
+
 # test_european_option_original()
 # test_american_option_original()
 # test_asian_option_original()
 # test_snowball_option_original()
 # test_knockout_option_original()
+# test_basket_snowball_option()
 
-scenario_extreme = PricingContext(
-    pricing_date='2021-08-13T00:00:00.000+0800',
-    spot=[{"symbol": "600067.SH", "value": 4.25}],
-    volatility=[{"symbol": "600067.SH", "value": 0.1}],
-    interest_rate=0.05,
-    dividend_yield=[{"symbol": "600067.SH", "value": 0.04}]
-)
+# scenario_extreme = PricingContext(
+#     pricing_date='2021-08-13T00:00:00.000+0800',
+#     spot=[{"symbol": "600067.SH", "value": 4.25}],
+#     volatility=[{"symbol": "600067.SH", "value": 0.1}],
+#     interest_rate=0.05,
+#     dividend_yield=[{"symbol": "600067.SH", "value": 0.04}]
+# )
 
 # def test_european_option_modified():
 #     assert round(european_option.calc(RiskMeasure.Price), 3) == 17.359
@@ -187,61 +191,68 @@ scenario_extreme = PricingContext(
 #     assert round(knockout_option.calc(RiskMeasure.EqRhoQ), 3) == -44001.311
 
 
-
 # print_result(snowball_option)
 
 # with scenario_extreme:
-    # test_european_option_modified()
-    # test_american_option_modified()
-    # test_asian_option_modified()
-    # test_snowball_option_modified()
-    # test_knockout_option_modified()
-    
+# test_european_option_modified()
+# test_american_option_modified()
+# test_asian_option_modified()
+# test_snowball_option_modified()
+# test_knockout_option_modified()
 
 
+##TODO：basket_snowball_option经with作用域后线程被killed，无法打印结果。
 
-##TODO#########################################
-# betas = np.ones(5) * 0.1
-# corr_matrix = betaVectorToCorrMatrix(betas)
+betas = np.ones(5) * 0.1
+corr_matrix = betaVectorToCorrMatrix(betas)
 
-# basket_snowball_option = BasketSnowballOption(option_type=OptionType.CALL,
-#                                               start_date=datetime.datetime(2020, 1, 1),
-#                                               expiry=datetime.datetime(2022, 1, 1),
-#                                               initial_spot=100,
-#                                               participation_rate=1.0,
-#                                               barrier=110,
-#                                               knock_in_price=88,
-#                                               notional=1000000,
-#                                               rebate=0.2,
-#                                               untriggered_rebate=0.2,
-#                                               underlier_symbol=["600277.SH", "600269.SH", "600201.SH", "600067.SH", "600243.SH"],
-#                                               knock_in_type='SPREADS',
-#                                               knock_in_strike1=5.3,
-#                                               knock_in_strike2=5.4,
-#                                               value_date=datetime.datetime(2021, 8, 13),
-#                                               currency=Currency.CNY,
-#                                               weights=[0.2, 0.2, 0.2, 0.2, 0.2],
-#                                               correlation_matrix=corr_matrix)
-# scenario_extreme = PricingContext(
-#     pricing_date='2015-01-01T00:00:00.000+0800',
-#     spot=[{"symbol": "600277.SH", "value": 100},
-#           {"symbol": "600269.SH", "value": 100},
-#           {"symbol": "600201.SH", "value": 100},
-#           {"symbol": "600067.SH", "value": 100},
-#           {"symbol": "600243.SH", "value": 100}],
-#     volatility=[{"symbol": "600277.SH", "value": 0.3},
-#                 {"symbol": "600269.SH", "value": 0.3},
-#                 {"symbol": "600201.SH", "value": 0.3},
-#                 {"symbol": "600067.SH", "value": 0.3},
-#                 {"symbol": "600243.SH", "value": 0.3}],
-#     interest_rate=0.05,
-#     dividend_yield=[{"symbol": "600277.SH", "value": 0.01},
-#                     {"symbol": "600269.SH", "value": 0.01},
-#                     {"symbol": "600201.SH", "value": 0.01},
-#                     {"symbol": "600067.SH", "value": 0.01},
-#                     {"symbol": "600243.SH", "value": 0.01}]
-# )
-# # print(basket_snowball_option.calc(RiskMeasure.Price))
-# with scenario_extreme:
-#     # print(basket_snowball_option.calc(RiskMeasure.Price))
-#     basket_snowball_option.calc(RiskMeasure.Price)
+
+basket_snowball_option = BasketSnowballOption(option_type=OptionType.CALL,
+                                            start_date=datetime.datetime(
+                                                2020, 1, 1),
+                                            expiry=datetime.datetime(
+                                                2022, 1, 1),
+                                            initial_spot=100,
+                                            participation_rate=1.0,
+                                            barrier=110,
+                                            knock_in_price=88,
+                                            notional=1000000,
+                                            rebate=0.2,
+                                            untriggered_rebate=0.2,
+                                            underlier_symbol=[
+                                                "600277.SH", "600269.SH", "600201.SH", "600067.SH", "600243.SH"],
+                                            knock_in_type='SPREADS',
+                                            knock_in_strike1=5.3,
+                                            knock_in_strike2=5.4,
+                                            value_date=datetime.datetime(
+                                                2021, 8, 13),
+                                            currency=Currency.CNY,
+                                            weights=[
+                                                0.2, 0.2, 0.2, 0.2, 0.2],
+                                            correlation_matrix=corr_matrix)
+
+scenario_extreme = PricingContext(
+    pricing_date='2015-01-01T00:00:00.000+0800',
+    spot=[{"symbol": "600277.SH", "value": 100},
+        {"symbol": "600269.SH", "value": 100},
+        {"symbol": "600201.SH", "value": 100},
+        {"symbol": "600067.SH", "value": 100},
+        {"symbol": "600243.SH", "value": 100}],
+    volatility=[{"symbol": "600277.SH", "value": 0.3},
+                {"symbol": "600269.SH", "value": 0.3},
+                {"symbol": "600201.SH", "value": 0.3},
+                {"symbol": "600067.SH", "value": 0.3},
+                {"symbol": "600243.SH", "value": 0.3}],
+    interest_rate=0.05,
+    dividend_yield=[{"symbol": "600277.SH", "value": 0.01},
+                    {"symbol": "600269.SH", "value": 0.01},
+                    {"symbol": "600201.SH", "value": 0.01},
+                    {"symbol": "600067.SH", "value": 0.01},
+                    {"symbol": "600243.SH", "value": 0.01}]
+)
+print(basket_snowball_option.calc(RiskMeasure.Price))
+with scenario_extreme:
+    print(basket_snowball_option.calc(RiskMeasure.Price))
+
+    # return (basket_snowball_option.calc(RiskMeasure.Price))
+# basket_snowball_option.calc(RiskMeasure.Price)
